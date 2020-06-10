becef151219c99a3a526f72cf2271f875918ed00
Dawud Rehan <d.rehan7@gmail.com>
2020-06-10 00:04:53 -0400

    init pygame window

--- game.py
+++ game.py
@@ -1,3 +1,25 @@
-def game():
-    run = 3
-    return run
+import pygame, sys
+from pygame.locals import *
+
+
+window_width, window_height = 800, 700
+
+def main():
+    global FPSCLOCK, window
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    window = pygame.display.set_mode((window_width, window_height))
+    pygame.display.set_caption('FUckY Sudoku')
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+
+
+if __name__ == "__main__":
+    main()
